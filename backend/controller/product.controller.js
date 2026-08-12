30ae0de47a73faa6bbcc0ee6a906e13af47d342e
Nisal Sandeep <sandeep-as22018@stu.kln.ac.lk>
2026-08-12 10:47:29 +0530

    feat: implement featured products retrieval with caching in Redis

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import { redis } from "../lib/redis.js";
 
 
 export const getAllProducts = async (req, res) => {
@@ -9,3 +10,31 @@ export const getAllProducts = async (req, res) => {
         res.status(500).json({ message: "Error fetching products" });
     }
 }
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    //if not in redis, fetch from database
+    // lean is used to return plain JavaScript objects instead of Mongoose documents, which can improve performance and reduce memory usage.
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    //store in redis for future requests
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.error("Error fetching featured products:", error);
+    res
+      .status(500)
+      .json({ message: "Error fetching products", error: error.message });
+  }
+};
