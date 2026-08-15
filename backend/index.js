e8e54b3388fc1a12edd5f278c32358a6b2c882d0
Nisal Sandeep <sandeep-as22018@stu.kln.ac.lk>
2026-08-16 00:19:12 +0530

    feat: implement cart management with routes and controller for adding, retrieving, and removing products

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import { connectDb } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 dotenv.config();
 
@@ -20,8 +21,10 @@ app.get("/health", (req, res) => {
 
 app.use(express.json());
 app.use(cookieParser())
+
 app.use("/api/auth",authRoutes );
 app.use("/api/products",productRoutes );
+app.use("/api/cart",cartRoutes );
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/routes/cart.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getCartProducts, addToCart, removeAllFromCart } from "../controller/cart.controller.js"
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+
+router.get("/", protectRoute, getCartProducts);
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
+
+
+export default router;
